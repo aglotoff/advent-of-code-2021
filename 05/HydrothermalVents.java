6c0443693a9b003dc4c3c24caf11f4963e95242b
Andrey Glotov <andrei.glotoff@gmail.com>
2021-12-06 08:57:31 +0300

    Remove warning in day 5

--- 05/HydrothermalVents.java
+++ 05/HydrothermalVents.java
@@ -1,7 +1,6 @@
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
